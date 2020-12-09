dad0c2ee6c11e063aadd299d0c32074d898e1471
Mateusz Drzymala <Mateusz@hybridservicesltd.local>
2020-12-09 12:18:04 +0000

    105

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
-const isDev = require('electron-is-dev');
 
 
 
-async function createWindow() {
+function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -16,9 +15,11 @@ async function createWindow() {
 
     win.loadFile('index.html');
 
-    win.once('ready-to-show', () => {
-        console.log('checkin for updates')
-        autoUpdater.checkForUpdatesAndNotify();
+    win.once('ready-to-show', async () => {
+       let checkNotify = await autoUpdater.checkForUpdatesAndNotify(); 
+      // let check = await autoUpdater.checkForUpdates();        
+       console.log('checkNotify:',checkNotify)
+       win.webContents.send('checkNotify', checkNotify);
     });
 
 
@@ -40,12 +41,23 @@ app.on('activate', () => {
     }
 })
 
-ipcMain.on('app_version', (event) => {
+ipcMain.on('app_version', (event) => {   
     event.sender.send('app_version', { version: app.getVersion() })
 })
 
-autoUpdater.on('update-available', () => {
-    win.webContents.send('update_available');
+autoUpdater.on('update-available', (event) => {
+    win.webContents.send('update_available', event);
+    event.sender.send('update_available', e);
+});
+
+autoUpdater.on('checking-for-update', (event) =>{
+    console.log(event)
+    //win.webContents.send('checking_for_updates', event);
+    
+})
+
+autoUpdater.on('error', (e) => {    
+    console.log('error',e)
 });
 
 
