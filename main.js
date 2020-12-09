066e1269aa296e59a8602b28166f20daaf973a46
Mateusz Drzymala <Mateusz@hybridservicesltd.local>
2020-12-09 10:53:30 +0000

    103

--- main.js
+++ main.js
@@ -3,16 +3,6 @@ const { autoUpdater } = require('electron-updater');
 const isDev = require('electron-is-dev');
 
 
-console.log(process.env.NODE_ENV)
-if (process.env.ELECTRON_IS_DEV !== 'production') {
-    require('dotenv').config();
-  }
-
-  if (isDev) {
-	console.log('Running in development');
-} else {
-	console.log('Running in production');
-}
 
 async function createWindow() {
     const win = new BrowserWindow({
@@ -55,14 +45,15 @@ ipcMain.on('app_version', (event) => {
 })
 
 autoUpdater.on('update-available', () => {
-    console.log('update_available!');
-    mainWindow.webContents.send('update_available');
+    win.webContents.send('update_available');
 });
 
+
 autoUpdater.on('update-downloaded', () => {
-    console.log('update_downloaded!');
-    mainWindow.webContents.send('update_downloaded');
+    win.webContents.send('update_downloaded');
 });
+
+
 
 ipcMain.on('restart_app', ()=>{
     autoUpdater.quitAndInstall();
